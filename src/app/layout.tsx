3eb6e63a695dadffbc4ae28d46ce5c447dcb7ee0
vace <ocdo@qq.com>
2025-04-22 02:56:46 +0800

    feat: add favicon and web app manifest for improved branding and user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,12 @@ export default async function RootLayout({
         suppressHydrationWarning
         className={clsx('antialiased', GeistSans.variable, GeistMono.variable)}
       >
+        <head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </head>
         <body className="min-h-screen flex flex-col relative">
           <ThemeProvider
             attribute="class"
